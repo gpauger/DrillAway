c339c5702ce624170b09bef523cdb972da116398
Greg Auger <gpauger@gmail.com>
2019-04-09 10:41:50 -0700

    add 'likes' drill

--- DrillAway/Program.cs
+++ DrillAway/Program.cs
@@ -10,6 +10,14 @@ namespace DrillAway
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(drills.Likes(new string[] { "Peter" }));
+            Console.ReadLine();
+            Console.WriteLine(drills.Likes(new string[] { "Jacob", "Alex" }));
+            Console.ReadLine();
+            Console.WriteLine(drills.Likes(new string[] { "Max", "John", "Mark" }));
+            Console.ReadLine();
+            Console.WriteLine(drills.Likes(new string[] { "Alex", "Jacob", "Mark", "Max" }));
+            Console.ReadLine();
         }
     }
 }
---
+++ DrillAway/drills.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DrillAway
+{
+    public static class drills
+    {
+
+        public static string Likes(string[] name)
+        {
+            int x = name.Count();
+            int rest = x - 2;
+            if (x == 0)
+            { return "no one likes this"; }
+            else if (x == 1)
+            { return name[0] + " likes this"; }
+            else if (x == 2)
+            { return name[0] + " and " + name[1] + " like this"; }
+            else if (x == 3)
+            { return name[0] + ", " + name[1] + " and " + name[2] + " like this"; }
+            else if (x > 3)
+            { return name[0] + ", " + name[1] + " and " + rest + " others like this"; }
+            else return "oops";
+        }
+    }
+}
+
